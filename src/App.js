3460688ea46a0374db0ba00ded6b12fd671968a2
Chris Tack <chris_tack@mac.com>
2020-10-22 10:59:44 -0700

    Pass props to child

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="container mt-4">
       <InputForm updateLeaderboardArray={updateLeaderboardArray} />
-      <OutputLeaderboard />
+      <OutputLeaderboard leaderboard={leaderboard} />
     </div>
   );
 }
